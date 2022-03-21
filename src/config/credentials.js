a35100d52554a36fbf0766eef03e149d808c4a05
admondtamang <admondtamang@gmail.com>
2022-03-21 14:38:07 +0545

    [add] update crediantials

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,7 +1,14 @@
+// const DATABASE = {
+//     DB_HOST: "localhost",
+//     DB_NAME: "db_leads",
+//     DB_USER: "root",
+//     DB_PASS: "password",
+// };
 const DATABASE = {
-    DB_HOST: "localhost",
+    DB_HOST: "http://35.154.133.111/",
     DB_NAME: "db_leads",
     DB_USER: "root",
-    DB_PASS: "password",
+    DB_PASS: "my-secret-pw",
 };
+
 export { DATABASE };
